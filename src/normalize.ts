2c006a09980ac9a0e84f2eeca4ad48bc813e3e30
Xiphe <github@xiphe.net>
2020-02-25 11:53:31 +0100

    fix: allow hex input for normalizeValues

--- src/normalize.ts
+++ src/normalize.ts
@@ -2,13 +2,17 @@ import rgbHex from 'rgb-hex';
 import namer from 'color-namer';
 
 export function normalize(value: string): string {
-  const match = value.match(/rgb\(.*\)/);
-  if (match) {
-    const hex = `#${rgbHex(match[0])}`;
-    const namedColor = namer(hex).html[0];
-    return value
-      .replace(/rgb\(.*\)/, namedColor.distance === 0 ? namedColor.name : hex)
-      .toLowerCase();
+  const rgbMatch = value.match(/rgb\(.*\)/);
+  if (rgbMatch) {
+    value = value.replace(/rgb\(.*\)/, `#${rgbHex(rgbMatch[0])}`);
+  }
+  const hexMatch = value.match(/(#[0-9a-f]{6})/i);
+  if (hexMatch) {
+    const namedColor = namer(hexMatch[0]).html[0];
+    return value.replace(
+      /(#[0-9a-f]{6})/i,
+      (namedColor.distance === 0 ? namedColor.name : hexMatch[0]).toLowerCase(),
+    );
   }
 
   return value;
