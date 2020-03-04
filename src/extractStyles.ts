1093dd03c3e881ac94890df958778608cdb613c5
Xiphe <github@xiphe.net>
2020-03-04 10:19:33 +0100

    refactor(getStyles): move extractStyles into own file
    in order to make more clear that this function is not executed in node

---
+++ src/extractStyles.ts
@@ -0,0 +1,15 @@
+type Styles = { [key: string]: string };
+
+/* This is executed in the browser */
+export function extractStyles(
+  element: Element,
+  styles: Styles[],
+  pseudoElt?: string,
+): Styles {
+  const computed = window.getComputedStyle(element, pseudoElt);
+
+  return styles.reduce((memo, { key, name }) => {
+    memo[name] = computed.getPropertyValue(key);
+    return memo;
+  }, {} as Styles);
+}
--- src/getStyles.ts
+++ src/getStyles.ts
@@ -1,6 +1,7 @@
 import { Page } from 'playwright';
 import kebabCase from 'lodash.kebabcase';
 import { normalize as defaultNormalize } from './normalize';
+import { extractStyles } from './extractStyles';
 
 export type Options = {
   normalize?: (value: string, prop: keyof CSSStyleDeclaration) => string;
@@ -13,24 +14,19 @@ export async function getStyles<T extends (keyof CSSStyleDeclaration)[]>(
   props: T,
   { normalize = defaultNormalize, pseudoElt }: Options,
 ): Promise<{ [key in T[0]]: string }> {
+  const styles = props.map((prop: any) => {
+    if (typeof prop !== 'string') {
+      throw new Error(`Unexpected prop ${prop} of type ${typeof prop}`);
+    }
+    return {
+      name: prop,
+      key: kebabCase(prop).toLowerCase(),
+    };
+  });
   const computedStyles = await page.$eval(
     selector,
-    (element, styles, pseudoElt) => {
-      const computed = window.getComputedStyle(element, pseudoElt);
-      return styles.reduce((memo, { key, name }) => {
-        memo[name] = computed.getPropertyValue(key);
-        return memo;
-      }, {} as { [key: string]: string });
-    },
-    props.map((prop: any) => {
-      if (typeof prop !== 'string') {
-        throw new Error(`Unexpected prop ${prop} of type ${typeof prop}`);
-      }
-      return {
-        name: prop,
-        key: kebabCase(prop).toLowerCase(),
-      };
-    }),
+    extractStyles,
+    styles,
     pseudoElt,
   );
 
