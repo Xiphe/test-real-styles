5786b3f09d23d0bd2580d8b99b0a25f39a2e9629
Xiphe <github@xiphe.net>
2022-10-20 10:44:37 +0200

    test: use lauch api instead of deprecated one in transition spec

--- src/__tests__/transiton.spec.tsx
+++ src/__tests__/transiton.spec.tsx
@@ -1,26 +1,28 @@
-import withRealStyles from '../index';
+import { launch } from '../index';
 
-withRealStyles(
-  `
-  .base {
-    border-bottom: 2px solid blue;
-    transition: border-color 1s 1s;
-  }
-  .active {
-    border-bottom: 2px solid fuchsia;
-  }
-  `,
-  ({ browserName, getStyles, updatePage }) => {
-    describe(`button in ${browserName}`, () => {
-      it('gets correct styles after transition', async () => {
-        const div = document.createElement('div');
-        div.className = 'base active';
-        await updatePage(div);
+describe('transition styles', () => {
+  test.each(['chromium' as const, 'webkit' as const, 'firefox' as const])(
+    'gets correct styles after transition %s',
+    async (browser) => {
+      const { updatePage, getStyles } = launch(
+        browser,
+        `
+        .base {
+          border-bottom: 2px solid blue;
+          transition: border-color 1s 1s;
+        }
+        .active {
+          border-bottom: 2px solid fuchsia;
+        }
+        `,
+      );
+      const div = document.createElement('div');
+      div.className = 'base active';
+      await updatePage(div);
 
-        expect(await getStyles(div, ['borderBottomColor'])).toEqual({
-          borderBottomColor: 'fuchsia',
-        });
+      expect(await getStyles(div, ['borderBottomColor'])).toEqual({
+        borderBottomColor: 'fuchsia',
       });
-    });
-  },
-);
+    },
+  );
+});
