75ee9ffaef9e35f77349a91e9945bc10bfcb2c53
Xiphe <github@xiphe.net>
2022-10-21 12:20:08 +0200

    fix: use setTimeout as polyfill
    it seems most stable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import 'setimmediate';
 export { default as launch } from './launch';
 export { default as getRealStyles } from './getRealStyles';
 export { toCss } from './toCss';
--- src/launch.ts
+++ src/launch.ts
@@ -4,6 +4,11 @@ import domToPlaywright from 'dom-to-playwright';
 import { getStyles, Options } from './getStyles';
 import { withTimeout } from './withTimeout';
 
+if (!(global as any).setImmediate) {
+  /** @see https://github.com/microsoft/playwright/issues/18243 */
+  (global as any).setImmediate = setTimeout;
+}
+
 export const PLAYWRIGHT_BROWSERS = [
   'chromium' as const,
   'firefox' as const,
@@ -28,23 +33,23 @@ export default function launchStyleBrowser(
   if (!cache[browserName]) {
     cache[browserName] = withTimeout(
       playwright[browserName].launch(),
-      `Failed to launch ${browserName} in under %n.\nTry reducing parallel test runs`,
+      `Failed to launch ${browserName} in under %n`,
     );
   }
   const browserP = cache[browserName];
   const contextP = withTimeout(
     browserP.then((b) => b.newContext()),
-    `Failed to create context in ${browserName} in under %n.\nTry reducing parallel test runs`,
+    `Failed to create context in ${browserName} in under %n`,
   );
 
   const pageP = withTimeout(
     contextP.then((c) => c.newPage()),
-    `Failed to create page in ${browserName} in under %n.\nTry reducing parallel test runs`,
+    `Failed to create page in ${browserName} in under %n`,
   );
   const dtpP = pageP.then(domToPlaywright);
   const stylesP = withTimeout(
     resolveStyleInput(stylesInput),
-    'Failed to resolve style input in under %n',
+    `Failed to resolve style input for ${browserName} in under %n`,
   );
 
   return {
@@ -62,18 +67,18 @@ export default function launchStyleBrowser(
 
       await withTimeout(
         update(element),
-        'Failed to update page in under %n.\nTry reducing parallel test runs',
+        `Failed to update page within ${browserName} in under %n`,
       );
 
       await withTimeout(
         page.addStyleTag(styles),
-        'Failed to add styles to page in under %n.\nTry reducing parallel test runs',
+        `Failed to add styles to page within ${browserName} in under %n`,
       );
 
       if (!transitions) {
         await withTimeout(
           page.addStyleTag({ content: DISABLE_TRANSITIONS }),
-          'Failed to disable transitions in under %n.\nTry reducing parallel test runs',
+          `Failed to disable transitions within ${browserName} in under %n`,
         );
       }
     },
@@ -87,7 +92,7 @@ export default function launchStyleBrowser(
 
       return withTimeout(
         getStyles(page, select(element), styles, options),
-        'Failed to get styles in under %n.\nTry reducing parallel test runs',
+        `Failed to get styles within ${browserName} in under %n`,
       );
     },
     async hover(element: HTMLElement) {
@@ -96,7 +101,7 @@ export default function launchStyleBrowser(
 
       await withTimeout(
         page.hover(select(element)),
-        'Failed to hover element in under %n.\nTry reducing parallel test runs',
+        `Failed to hover element within ${browserName} in under %n`,
       );
     },
     async focus(element: HTMLElement) {
@@ -105,7 +110,7 @@ export default function launchStyleBrowser(
 
       await withTimeout(
         page.focus(select(element)),
-        'Failed to focus element in under %n.\nTry reducing parallel test runs',
+        `Failed to focus element within ${browserName} in under %n`,
       );
     },
   };
