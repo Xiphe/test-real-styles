29d1e53fc60f7950817933f10f6e955c429e9ab8
Xiphe <github@xiphe.net>
2023-07-03 11:15:00 +0200

    fix: upgrade dependencies

--- src/__tests__/fixtures/Headline.ts
+++ src/__tests__/fixtures/Headline.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export default styled.h1<{ blue?: boolean }>`
+export default styled.h1.withConfig({
+  shouldForwardProp(p) {
+    return !['blue'].includes(p);
+  },
+})<{ blue?: boolean }>`
   background-color: ${({ blue }) => (blue ? 'blue' : 'fuchsia')};
   &:hover {
     background-color: red;
